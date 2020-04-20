f49faad3f6fb55b1014d2c1eb40ca7cb2cf01f08
dotunpeters <oyelakindotun@gmail.com>
2020-04-20 21:30:16 +0100

    day 1 coding challenge

--- day1.py
+++ day1.py
@@ -1,7 +1,4 @@
 
 def twofer(name=None):
     # Enter your code here. Read input from STDIN. Print output to STDOUT
-    if name == None:
-        return "One for you, one for me."
-    else:
-        return f"One for {name}, one for me."
+    pass
